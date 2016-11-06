159485f6db8f1ad4d7cd6cf04bbb1bbfcd555745
Mike Callaghan <Michael.D.Callaghan@Disney.com>
2016-11-06 07:26:39 -0500

    Initial project set up.

---
+++ server/main.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const app = electron.app;
+const Window = electron.BrowserWindow;
+
+app.on('ready', _ => {
+  console.log('hello, world');
+  console.log(process.env);
+
+  let mainWindow;
+
+  mainWindow = new Window(
+    {
+      height: 640,
+      width: 480
+    }
+  );
+
+  mainWindow.on('closed', _ => {
+    console.log('mainWindow closed');
+    mainWindow = null;
+  });
+
+  if (process.env.WATCH) {
+    mainWindow.loadURL('http://localhost:8100');
+  } else {
+    mainWindow.loadURL('file://www/index.html');
+  }
+});
