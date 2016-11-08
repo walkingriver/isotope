131030f6653bd385f6b3ae00718314d50591f287
Mike Callaghan <Michael.D.Callaghan@Disney.com>
2016-11-08 09:40:21 -0500

    Removed unused config.

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -1,7 +1,2 @@
-function isHeroku()
-{
-    // Rudimentary check to see if we are running on Heroku. Should provide a more flexible config.
-    return window.location.hostname.indexOf('herokuapp.com') > 0;
-}
 
-export const serverUrl = isHeroku() ? '/' : 'http://conveyantsentry.azurewebsites.net/api';
+export const serverUrl = 'http://conveyantsentry.azurewebsites.net/api';
